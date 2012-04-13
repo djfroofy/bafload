d0416dc16c9e3ff1ba7bd05e312754b7ede92323
Drew Smathers <drew.smathers@gmail.com>
2012-04-13 11:50:21 -0400

    simplified SlidingStats impl and more unit tests

--- bafload/stats.py
+++ bafload/stats.py
@@ -1,9 +1,19 @@
-import time
+import math
 from collections import deque
 
-class SlidingStats(object):
+from twisted.internet import reactor
 
-    def __init__(self, t, slot_duration_secs=5, size=1024):
+
+class SlidingStats(object):
+    """
+    A sliding view of time a single floating point where time is descretely
+    divided into slots of duration C{slot_duration_secs} and number of slots is
+    C{size}. This should be initialized for a given time C{t} (float seconds)
+    provided by the wizard. The window also has the property that there are no
+    gaps between each time slot.
+    """
+
+    def __init__(self, t, slot_duration_secs=1, size=2048):
         self.slot_duration_secs = slot_duration_secs
         self.size = size
         self.generation = size * self.slot_duration_secs
@@ -12,47 +22,55 @@ class SlidingStats(object):
 
     def _reset_slots(self, t):
         dur = self.slot_duration_secs
-        size = self.size
-        start = dur + t - self.generation
-        self.slots = deque([(start + (i * dur), 0) for i in xrange(size)])
+        n = self.size
+        t0 = dur + t - self.generation
+        self.slots = deque([(t0 + (i * dur), 0) for i in xrange(n)], n)
 
-    def update(self, t, count, current=False):
-        t = t - (t % self.slot_duration_secs)
-        if current and (t - self.slots[-1][0]) > self.generation:
-            self._reset_slots(t)
-        if current:
-            while (t - self.slots[0][0]) > self.generation:
-                self._extend()
-            while (t - self.slots[-1][0]) >= self.slot_duration_secs:
-                self._extend()
+    def update(self, t, count):#, current=False):
+        """
+        Add C{count} the count at the base of time t (t') where t' is given by:
+        
+        t' == t  - t % slot_duration_secs
+
+        If t' is older than our earliest time in the window this is a noop. If
+        t' is newer than than our most current time (t_k) then slots t_k
+        through t_n are added where t_n == t' - slot_duration_secs.
+
+        @param t: The time to update for as C{float}
+        @param count: The value to add to current total for t' (if in window)
+        """
+        tp = t - (t % self.slot_duration_secs)
+        if tp < self.slots[0][0]:
+            return
+        if (tp - self.slots[-1][0]) > self.generation:
+            self._reset_slots(tp)
         index = -1
         (last, v) = self.slots[index]
-        while t < last:
-            index -= 1
-            try:
-                (last, v) = self.slots[index]
-            except IndexError:
-                return
-        self.slots[index] = (t, v + count)
-
-    def _extend(self):
-        last = self.slots[-1][0]
-        self.slots.append((last + self.slot_duration_secs, 0))
-        self.slots.popleft()
+        while tp > last:
+            self.slots.append((last + self.slot_duration_secs, 0))
+            (last, v) = self.slots[index]
+        if tp < last:
+            index = int(((tp - last) / self.slot_duration_secs) - 1)
+        tk, v = self.slots[index]
+        self.slots[index] = (tp, v + count)
 
 
 class ThroughputCounter(object):
     cutoff = 3600
 
-    def __init__(self):
+    def __init__(self, clock=None):
+        if clock is None:
+            clock = reactor
+        self.stats = SlidingStats(clock.seconds())
         self.starts = {}
+        self.clock = clock
 
     def start_entity(self, id):
-        t = time.time()
+        t = self.clock.seconds()
         self.starts[id] = t
 
     def stop_entity(self, id, size):
-        t1 = time.time()
+        t1 = self.clock.seconds()
         t0 = self.starts.pop(id)
         total = t1 - t0
         if total < 5:
--- bafload/test/test_stats.py
+++ bafload/test/test_stats.py
@@ -1,3 +1,5 @@
+from pprint import pformat
+
 from twisted.trial.unittest import TestCase
 
 from bafload.stats import SlidingStats
@@ -5,8 +7,8 @@ from bafload.stats import SlidingStats
 class SlidingStatsTestCase(TestCase):
 
     def test_initial_slots(self):
-        stats = SlidingStats(200, 2, 100)
-        expected = range(2, 202, 2)
+        stats = SlidingStats(200.0, 2, 100)
+        expected = map(float, range(2, 202, 2))
         expected = zip(expected, [0] * 100)
         self.assertEquals(list(stats.slots), expected)
         self.assertEquals(stats.size, 100)
@@ -14,23 +16,35 @@ class SlidingStatsTestCase(TestCase):
         self.assertEquals(stats.generation, 200)
 
     def test_update(self):
-        stats = SlidingStats(400, 2, 100)
-        stats.update(400, 5, True)
-        stats.update(400, 14, True)
-        window = range(202, 402, 2)
+        stats = SlidingStats(400.0, 2, 100)
+        stats.update(400, 5)
+        stats.update(400, 14)
+        window = map(float, range(202, 402, 2))
         expected = zip(window, [0] * 99 + [19])
         self.assertEquals(list(stats.slots), expected)
-        stats.update(300, 6)
+        stats.update(300., 6)
         stats.update(302.5, 8)
         stats.update(301.5, 4)
-        expected = zip(window, ([0] * 49) + [10, 8] + ([0] * 48) + [19])
+        expected = zip(window, ([0] * 49) + [10., 8.] + ([0] * 48) + [19])
         self.assertEquals(list(stats.slots), expected)
-        stats.update(20, 200)
+        stats.update(20., 200)
+        self.assertEquals(list(stats.slots), expected)
+
+    def test_reset_when_past_generation(self):
+        stats = SlidingStats(200.0, 2, 100)
+        stats.update(900, 10)
+        window = map(float, range(702, 902, 2))
+        expected = zip(window, [0] * 99 + [10])
         self.assertEquals(list(stats.slots), expected)
 
     def test_slide_on_update(self):
         stats = SlidingStats(200, 2, 100)
-        stats.update(350, 7, True)
-        window = range(152, 352, 2)
+        stats.update(350., 7)
+        window = map(float, range(152, 352, 2))
         expected = zip(window, [0] * 99 + [7])
         self.assertEquals(list(stats.slots), expected)
+
+
+class ThroughputCounterTestCase(TestCase):
+    pass
+
